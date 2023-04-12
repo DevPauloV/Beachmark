c42d870a16101c8e77072cf050f405d4a2e241c4
Paulo Victor <vitorcanalle@gmail.com>
2023-04-12 10:24:28 -0300

    Add files via upload

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,428 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.TexProcessador = New System.Windows.Forms.TextBox()
+        Me.TexIdent = New System.Windows.Forms.TextBox()
+        Me.TexVersion = New System.Windows.Forms.TextBox()
+        Me.TexName = New System.Windows.Forms.TextBox()
+        Me.TexUser = New System.Windows.Forms.TextBox()
+        Me.TexFolder = New System.Windows.Forms.TextBox()
+        Me.TexCurrent = New System.Windows.Forms.TextBox()
+        Me.TexLinguage = New System.Windows.Forms.TextBox()
+        Me.TexTMemory = New System.Windows.Forms.TextBox()
+        Me.TexAMemory = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TexSpeed = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label1.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(157, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Processor:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label2.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(159, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Identifer:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label3.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(101, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Windows Version:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label4.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(113, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(115, 18)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Computer Name:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label5.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label5.Location = New System.Drawing.Point(149, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 18)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "UserName:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label6.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label6.Location = New System.Drawing.Point(109, 171)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(119, 18)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Windows Folder:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label7.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label7.Location = New System.Drawing.Point(121, 200)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(107, 18)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Current Folder:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.White
+        Me.Label8.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label8.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label8.Location = New System.Drawing.Point(363, 328)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(141, 18)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Total Ram Available:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.Color.White
+        Me.Label9.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label9.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label9.Location = New System.Drawing.Point(363, 299)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(135, 18)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "Total RAM Memory:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.White
+        Me.Label10.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label10.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label10.Location = New System.Drawing.Point(187, 299)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(71, 18)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "CPU: 10%"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.White
+        Me.Label11.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label11.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label11.Location = New System.Drawing.Point(159, 328)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(87, 18)
+        Me.Label11.TabIndex = 13
+        Me.Label11.Text = "RAM: 1,2 GB"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button2.Location = New System.Drawing.Point(623, 407)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 31)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Location = New System.Drawing.Point(530, 407)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.BackColor = System.Drawing.Color.White
+        Me.Label12.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label12.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label12.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label12.Location = New System.Drawing.Point(154, 226)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(74, 18)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "Language:"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(257, 294)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar1.TabIndex = 15
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(257, 323)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar2.TabIndex = 16
+        '
+        'TexProcessador
+        '
+        Me.TexProcessador.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexProcessador.Location = New System.Drawing.Point(234, 26)
+        Me.TexProcessador.Name = "TexProcessador"
+        Me.TexProcessador.Size = New System.Drawing.Size(488, 16)
+        Me.TexProcessador.TabIndex = 17
+        '
+        'TexIdent
+        '
+        Me.TexIdent.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexIdent.Location = New System.Drawing.Point(234, 55)
+        Me.TexIdent.Name = "TexIdent"
+        Me.TexIdent.Size = New System.Drawing.Size(488, 16)
+        Me.TexIdent.TabIndex = 18
+        '
+        'TexVersion
+        '
+        Me.TexVersion.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexVersion.Location = New System.Drawing.Point(234, 84)
+        Me.TexVersion.Name = "TexVersion"
+        Me.TexVersion.Size = New System.Drawing.Size(488, 16)
+        Me.TexVersion.TabIndex = 19
+        '
+        'TexName
+        '
+        Me.TexName.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexName.Location = New System.Drawing.Point(234, 113)
+        Me.TexName.Name = "TexName"
+        Me.TexName.Size = New System.Drawing.Size(488, 16)
+        Me.TexName.TabIndex = 20
+        '
+        'TexUser
+        '
+        Me.TexUser.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexUser.Location = New System.Drawing.Point(234, 142)
+        Me.TexUser.Name = "TexUser"
+        Me.TexUser.Size = New System.Drawing.Size(488, 16)
+        Me.TexUser.TabIndex = 21
+        '
+        'TexFolder
+        '
+        Me.TexFolder.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexFolder.Location = New System.Drawing.Point(234, 171)
+        Me.TexFolder.Name = "TexFolder"
+        Me.TexFolder.Size = New System.Drawing.Size(488, 16)
+        Me.TexFolder.TabIndex = 22
+        '
+        'TexCurrent
+        '
+        Me.TexCurrent.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexCurrent.Location = New System.Drawing.Point(234, 200)
+        Me.TexCurrent.Name = "TexCurrent"
+        Me.TexCurrent.Size = New System.Drawing.Size(488, 16)
+        Me.TexCurrent.TabIndex = 23
+        '
+        'TexLinguage
+        '
+        Me.TexLinguage.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexLinguage.Location = New System.Drawing.Point(234, 226)
+        Me.TexLinguage.Name = "TexLinguage"
+        Me.TexLinguage.Size = New System.Drawing.Size(488, 16)
+        Me.TexLinguage.TabIndex = 24
+        '
+        'TexTMemory
+        '
+        Me.TexTMemory.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexTMemory.Location = New System.Drawing.Point(495, 302)
+        Me.TexTMemory.Name = "TexTMemory"
+        Me.TexTMemory.Size = New System.Drawing.Size(227, 16)
+        Me.TexTMemory.TabIndex = 25
+        '
+        'TexAMemory
+        '
+        Me.TexAMemory.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexAMemory.Location = New System.Drawing.Point(505, 328)
+        Me.TexAMemory.Name = "TexAMemory"
+        Me.TexAMemory.Size = New System.Drawing.Size(217, 16)
+        Me.TexAMemory.TabIndex = 26
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'TexSpeed
+        '
+        Me.TexSpeed.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TexSpeed.Location = New System.Drawing.Point(229, 253)
+        Me.TexSpeed.Name = "TexSpeed"
+        Me.TexSpeed.Size = New System.Drawing.Size(488, 16)
+        Me.TexSpeed.TabIndex = 28
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.BackColor = System.Drawing.Color.White
+        Me.Label13.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label13.Font = New System.Drawing.Font("Sitka Small", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label13.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label13.Location = New System.Drawing.Point(172, 248)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(51, 18)
+        Me.Label13.TabIndex = 27
+        Me.Label13.Text = "Speed:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BackgroundImage = Global.WinFormsApp2.My.Resources.Resources._1c7e280e30f47020de1845cc83b37c91
+        Me.ClientSize = New System.Drawing.Size(734, 450)
+        Me.Controls.Add(Me.TexSpeed)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.TexAMemory)
+        Me.Controls.Add(Me.TexTMemory)
+        Me.Controls.Add(Me.TexLinguage)
+        Me.Controls.Add(Me.TexCurrent)
+        Me.Controls.Add(Me.TexFolder)
+        Me.Controls.Add(Me.TexUser)
+        Me.Controls.Add(Me.TexName)
+        Me.Controls.Add(Me.TexVersion)
+        Me.Controls.Add(Me.TexIdent)
+        Me.Controls.Add(Me.TexProcessador)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "CPU INFO"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents TexProcessador As TextBox
+    Friend WithEvents TexIdent As TextBox
+    Friend WithEvents TexVersion As TextBox
+    Friend WithEvents TexName As TextBox
+    Friend WithEvents TexUser As TextBox
+    Friend WithEvents TexFolder As TextBox
+    Friend WithEvents TexCurrent As TextBox
+    Friend WithEvents TexLinguage As TextBox
+    Friend WithEvents TexTMemory As TextBox
+    Friend WithEvents TexAMemory As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TexSpeed As TextBox
+    Friend WithEvents Label13 As Label
+End Class
---
+++ WinFormsApp2/WinFormsApp2/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        GetSystemInfo()
+        GetMemoryInfo()
+
+    End Sub
+
+
+    Private Sub GetSystemInfo()
+        Dim Freq As String
+
+        TexVersion.Text = Environment.OSVersion.ToString()
+        TexName.Text = Environment.MachineName
+        TexProcessador.Text = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\SYSTEM\CentralProcessor\0", "ProcessorNameString", Nothing)
+        TexUser.Text = Environment.UserName
+        TexFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.System)
+        TexCurrent.Text = Environment.CurrentDirectory
+        TexIdent.Text = My.Computer.Registry.LocalMachine.OpenSubKey(
+    "HARDWARE\DESCRIPTION\System\CentralProcessor\0").GetValue("Identifier")
+        TexLinguage.Text = My.Computer.Info.InstalledUICulture.DisplayName
+
+        Freq = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\SYSTEM\CentralProcessor\0", "~MHz", Nothing)
+        TexSpeed.Text = Freq & " MHz"
+
+    End Sub
+
+
+
+    Private Sub GetMemoryInfo()
+        Const un = 1024 * 1024 * 1024 'KB / MB / GB
+
+        Dim memTotal = Val(My.Computer.Info.TotalPhysicalMemory)
+        Dim memDisponivel = Val(My.Computer.Info.AvailablePhysicalMemory)
+
+        TexTMemory.Text = Math.Round(memTotal / un, 2)
+        TexAMemory.Text = Math.Round(memDisponivel / un, 2)
+    End Sub
+
+
+
+    Private PerCounter As System.Diagnostics.PerformanceCounter
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim nutzram As Double
+        nutzram = (My.Computer.Info.TotalPhysicalMemory - My.Computer.Info.AvailablePhysicalMemory) / 1048576 / 1024
+        Label11.Text = "RAM: " & nutzram.ToString("N") & " GB"
+
+
+        Dim Prozent1 As Long
+        Prozent1 = My.Computer.Info.AvailablePhysicalMemory * 100
+        Dim Prozentsatz As Long
+        Prozentsatz = Val(Prozent1 / My.Computer.Info.TotalPhysicalMemory)
+        ProgressBar2.Value = Prozentsatz
+
+
+        Dim i As Integer = Integer.Parse(Format(PerCounter.NextValue, "##0"))
+        ProgressBar1.Value = i
+
+
+        Label10.Text = "CPU: " & i & " %"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PerCounter = New System.Diagnostics.PerformanceCounter
+        PerCounter.CategoryName = "Processor"
+        PerCounter.CounterName = "% Processor Time"
+        PerCounter.InstanceName = "_Total"
+    End Sub
+
+
+End Class
